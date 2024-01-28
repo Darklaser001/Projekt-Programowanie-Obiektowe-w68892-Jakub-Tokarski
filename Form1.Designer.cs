81380ec12ae88a01ee30db01cef5de7a6d111bb9
Darklaser001 <jakubtokarski@onet.eu>
2024-01-28 20:17:46 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Projekt_Programowanie_Obiektowe_w68892_Jakub_Tokarski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(879, 87);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Tekst zapytania do bazy";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(897, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 149);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wyślij zapytanie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(879, 316);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(897, 167);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(213, 219);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Wrzuć plik do importu";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(897, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 149);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Importuj dane";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 108);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exportuj dane";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 547);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GUI Bazy Danych Fitness";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+
+namespace Projekt_Programowanie_Obiektowe_w68892_Jakub_Tokarski
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            runQuery(textBox1.Text, 0);
+        }
+
+        private void runQuery(string query, int todo)
+        {
+            if (query == "")
+            {
+                MessageBox.Show("Proszę wpisać zapytanie!");
+                return;
+            }
+
+            string MySQLConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=baza_fitness";
+
+            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            if (todo == 0)
+            {
+                try
+                {
+                    databaseConnection.Open();
+
+                    MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                    if (myReader.HasRows)
+                    {
+
+                        MessageBox.Show("Twoje zapytanie wygenerowało listę.");
+
+
+                        DataTable dt = new DataTable();
+                        dt.Load(myReader);
+                        dataGridView1.DataSource = dt;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Zapytanie wykonane sukcesywnie!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd zapytania: " + ex.Message);
+
+                }
+            }
+
+            if (todo == 1)
+            {
+                try
+                {
+                    databaseConnection.Open();
+
+                    MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                    
+                    string exportPath = @"C:\Users\Jakub\Desktop\";
+                    string exportCsv = "Export_dane.csv";
+                    StreamWriter csvFile = null;
+
+                    csvFile = new StreamWriter(@exportPath + exportCsv);
+
+                    for (int i = 0; i< myReader.FieldCount; i++)
+                    {
+                        csvFile.Write(String.Format("\"{0}\",",myReader.GetName(i)));
+                    }
+                    csvFile.Write(String.Format("\"{0}\" \n", myReader.GetName(myReader.FieldCount-1)));
+
+                    while (myReader.Read())
+                    {
+                        for (int i = 0; i < myReader.FieldCount; i++)
+                        {
+                            csvFile.Write(String.Format("\"{0}\",", myReader[i]));
+                        }
+                        csvFile.Write(String.Format("\"{0}\" \n", myReader[myReader.FieldCount - 1]));
+                    }
+
+                    csvFile.Close();
+                    MessageBox.Show("Plik eksportowany pomyślnie!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd zapytania lub zapisu pliku: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.AllowDrop = true;
+            richTextBox1.DragDrop += RichTextBox1_DragDrop;
+        }
+
+        private void RichTextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if(data!=null)
+            {
+                var fileNames = data as string[];
+                if(fileNames.Length>0)
+                {
+                    richTextBox1.Text = "";
+                    richTextBox1.Text = fileNames[0] + "\n";
+                    System.IO.StreamReader sr = new System.IO.StreamReader(fileNames[0]);
+                    richTextBox1.Text += sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("lol");
+            richTextBox1.Text = richTextBox1.Text.Replace("\"", "'");
+            string[] csv = richTextBox1.Text.Split('\n');
+            string values = csv[1].Replace("'", "`");
+            string[] table = csv[0].Split('\\');
+            table = table[table.Length-1].Split('.');
+
+            string query = "insert into `" + table[0] + "`(" + values + ") values \n";
+
+            for (int i = 2; i < csv.Length - 2; i++)
+            {
+                query += "(" + csv[i] + "), \n";
+            }
+            richTextBox1.Text = "";
+            runQuery(query + "(" + csv[csv.Length - 2] + ");", 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            runQuery(textBox1.Text, 1);
+        }
+    }
+}
